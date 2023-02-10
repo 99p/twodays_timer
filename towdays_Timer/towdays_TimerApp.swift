abf8220f7d7b4c6bc8e19fc00c9b751b2f46bafc
99p <poccori@gmail.com>
2023-02-10 11:43:55 +0900

    Initial Commit

---
+++ towdays_Timer/towdays_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  towdays_TimerApp.swift
+//  towdays_Timer
+//
+//  Created by macboy on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct towdays_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
